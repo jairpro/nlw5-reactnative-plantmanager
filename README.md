4602aa8c9d20aa63973d60dfb1d7d7f3b6e6e508
jairpro <jairedias@yahoo.com.br>
2021-04-21 22:55:45 -0300

    Aula 2

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # NLW#5 - Trilha React Native - Plant Manager
 Aplicativo para rega de plantas
+
+### Expo Icons
+```shell
+expo install @expo/vector-icons
+```
+[https://docs.expo.io/guides/icons/](https://docs.expo.io/guides/icons/)
+
+#### Família de ícones:
+[https://github.com/oblador/react-native-vector-icons/blob/master/README.md#bundled-icon-sets](https://github.com/oblador/react-native-vector-icons/blob/master/README.md#bundled-icon-sets)
+
+### Expo App Loading
+```shell
+expo install expo-app-loading
+```
+### React Nativation
+[Installation](https://reactnavigation.org/docs/getting-started#installing-dependencies-into-an-expo-managed-project)
+```shell
+yarn add @react-navigation/native
+```
+[Installing dependencies into a bare React Native project](https://reactnavigation.org/docs/getting-started#installing-dependencies-into-a-bare-react-native-project)
+```shell
+yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+```
+[Installing the stack navigator library](https://reactnavigation.org/docs/hello-react-navigation/#installing-the-stack-navigator-library)
+```shell
+yarn add @react-navigation/stack
+```
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,52 +1,86 @@
 import React from 'react'
-import { View, Text, Image, StyleSheet } from 'react-native'
-import Constants from 'expo-constants'
+import { 
+  View, 
+  Text, 
+  Image, 
+  StyleSheet, 
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+} from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useNavigation } from '@react-navigation/core'
 
 import watering from '../assets/watering.png'
 import colors from '../styles/colors'
-import { Button } from '../components/Button'
-
-const statusBarHeight = Constants.statusBarHeight
+import fonts from '../styles/fonts'
 
 export function Welcome() {
+  const navigation = useNavigation()
+
+  function handleStart() {
+    navigation.navigate('UserIdentification')
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas de {'\n'}
-        forma fácil
-      </Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
+        </Text>
 
-      <Image source={watering} style={styles.image}/>
+        <Image 
+          source={watering} 
+          style={styles.image}
+          resizeMode='contain'
+        />
 
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
 
-      <Button title={`>`} />
-    </View>
+        <TouchableOpacity 
+          style={styles.button} 
+          activeOpacity={0.7}
+          onPress={handleStart}
+        >
+          <Feather
+            name="chevron-right" 
+            style={styles.buttonIcon}
+          />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  
+  wapper: {
+    flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between' 
+    justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
 
   title: {
-    marginTop: statusBarHeight + 38,
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    marginTop: 38,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
 
   image: {
-    width: 292,
-    height: 284, 
+    height: Dimensions.get('window').width * 0.7, 
   },
 
   subtitle: {
@@ -54,5 +88,21 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
+  },
+
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+
+  buttonIcon: {
+    fontSize: 32,
+    color: colors.white,
   },
 })
