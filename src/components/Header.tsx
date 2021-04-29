59e51b6c7fe2fb45f0cd16183a53e489f864332b
jairpro <jairedias@yahoo.com.br>
2021-04-28 21:40:33 -0300

    Proporção 1:1 e somente imagens para usuário

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -46,9 +46,9 @@ export function Header() {
 
   const handleImageSelect = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
-      aspect: [4, 3],
+      aspect: [1, 1],
       quality: 1,
     })
 
